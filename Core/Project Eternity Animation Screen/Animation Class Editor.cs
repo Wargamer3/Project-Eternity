e7c0ef65c1f954f888a8943edec4e3770100a914
Wargamer <759249+Wargamer3@users.noreply.github.com>
2021-05-03 09:15:07 -0400

    Center the placeholder of the Marker Animation Timeline

--- Core/Project Eternity Animation Screen/Animation Class Editor.cs
+++ Core/Project Eternity Animation Screen/Animation Class Editor.cs
@@ -206,7 +206,7 @@ namespace ProjectEternity.GameScreens.AnimationScreen
                 if (ActiveLayer.ListActiveMarker[M].Sprite != null)
                 {
                     g.Draw(ActiveLayer.ListActiveMarker[M].Sprite, new Vector2(ActiveLayer.ListActiveMarker[M].Position.X, ActiveLayer.ListActiveMarker[M].Position.Y),
-                        null, Color.White, 0, Vector2.Zero, ActiveLayer.ListActiveMarker[M].ScaleFactor, SpriteEffects.None, 1);
+                        null, Color.White, 0, new Vector2(ActiveLayer.ListActiveMarker[M].Sprite.Width / 2, ActiveLayer.ListActiveMarker[M].Sprite.Height / 2), ActiveLayer.ListActiveMarker[M].ScaleFactor, SpriteEffects.None, 1);
                 }
             }
 
