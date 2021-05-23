ea9d4e5434bc75ee0d4332df3c1a382324f019f5
Wargamer <759249+Wargamer3@users.noreply.github.com>
2021-05-23 10:55:45 -0400

    Allow the Use Combo AI script to specify a weapon index

--- Mods/Project Eternity Triple Thunder AI/Evaluators/Shoot.cs
+++ Mods/Project Eternity Triple Thunder AI/Evaluators/Shoot.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework;
 
 namespace ProjectEternity.GameScreens.TripleThunderScreen
 {
-
     public sealed partial class GameScriptHolder
     {
         public class Shoot : TripleThunderScript, ScriptEvaluator
--- Mods/Project Eternity Triple Thunder AI/Evaluators/Use Combo.cs
+++ Mods/Project Eternity Triple Thunder AI/Evaluators/Use Combo.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
 using ProjectEternity.Core.AI;
 using Microsoft.Xna.Framework;
 
 namespace ProjectEternity.GameScreens.TripleThunderScreen
 {
-
     public sealed partial class GameScriptHolder
     {
         public class UseCombo : TripleThunderScript, ScriptEvaluator
         {
+            private int _WeaponIndex;
+
             public UseCombo()
                 : base(100, 50, "Use Combo", new string[0], new string[0])
             {
+                _WeaponIndex = -1;
             }
 
             public void Evaluate(GameTime gameTime, object Input, out bool IsCompleted, out List<object> Result)
@@ -19,15 +23,49 @@ namespace ProjectEternity.GameScreens.TripleThunderScreen
                 RobotAnimation CurrentRobot = Info.Owner;
 
                 CurrentRobot.ActiveAttackStance = "Walking";
-                CurrentRobot.UseCombo(gameTime, AttackInputs.LightPress);
+
+                if (_WeaponIndex == -1)
+                {
+                    CurrentRobot.UseCombo(gameTime, AttackInputs.LightPress);
+                }
+                else
+                {
+                    CurrentRobot.UseCombo(gameTime, AttackInputs.LightPress, CurrentRobot.Weapons.ActivePrimaryWeapons[_WeaponIndex], false);
+                }
 
                 Result = new List<object>();
                 IsCompleted = true;
             }
 
+            public override void Load(BinaryReader BR)
+            {
+                base.Load(BR);
+                _WeaponIndex = BR.ReadInt32();
+            }
+
+            public override void Save(BinaryWriter BW)
+            {
+                base.Save(BW);
+                BW.Write(_WeaponIndex);
+            }
+
             public override AIScript CopyScript()
             {
                 return new UseCombo();
+            }
+
+            [CategoryAttribute("Script Attributes"),
+            DescriptionAttribute("")]
+            public int WeaponIndex
+            {
+                get
+                {
+                    return _WeaponIndex;
+                }
+                set
+                {
+                    _WeaponIndex = value;
+                }
             }
         }
     }
