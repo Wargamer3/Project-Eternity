5d3108e48f9fd344aba37e70479bfcdd2910efa3
Wargamer <759249+Wargamer3@users.noreply.github.com>
2021-11-30 17:28:56 -0500

    Add a victory menu and victory script

---
+++ Mods/Project Eternity Deathmatch Map Scripts/Cutscene Scripts/Victory Screen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using ProjectEternity.Core;
+using ProjectEternity.Core.Scripts;
+
+namespace ProjectEternity.GameScreens.DeathmatchMapScreen
+{
+    public sealed partial class DeathmatchCutsceneScriptHolder
+    {
+        public class ScriptVictoryScreen : DeathmatchMapScript
+        {
+            public ScriptVictoryScreen()
+                : this(null)
+            {
+            }
+
+            public ScriptVictoryScreen(DeathmatchMap Map)
+                : base(Map, 100, 50, "Victory Screen", new string[0], new string[0])
+            {
+            }
+
+            public override void ExecuteTrigger(int Index)
+            {
+                IsEnded = true;
+                Owner.PushScreen(new VictoryMenu());
+            }
+
+            public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Draw(CustomSpriteBatch g)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Load(BinaryReader BR)
+            {
+            }
+
+            public override void Save(BinaryWriter BW)
+            {
+            }
+
+            protected override CutsceneScript DoCopyScript()
+            {
+                return new ScriptVictoryScreen(Map);
+            }
+
+            #region Properties
+
+            #endregion
+        }
+    }
+}
---
+++ Mods/Project Eternity Deathmatch Map/Menus/Victory Menu.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using ProjectEternity.Core;
+using ProjectEternity.Core.ControlHelper;
+using ProjectEternity.GameScreens.AnimationScreen;
+
+namespace ProjectEternity.GameScreens.DeathmatchMapScreen
+{
+    public class VictoryMenu : GameScreen
+    {
+        #region Ressources
+
+        #endregion
+
+        #region Variables
+
+        private SimpleAnimation VictoryAnimation;
+
+        #endregion
+
+        public VictoryMenu()
+        {
+        }
+
+        public override void Load()
+        {
+            VictoryAnimation = new SimpleAnimation();
+            VictoryAnimation.IsAnimated = true;
+            VictoryAnimation.Path = "Game End";
+            VictoryAnimation.Load(Content, string.Empty);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (VictoryAnimation.ActiveAnimation.ActiveKeyFrame + 1 < VictoryAnimation.ActiveAnimation.LoopEnd)
+            {
+                VictoryAnimation.Update(gameTime);
+            }
+
+            if (InputHelper.InputConfirmPressed())
+            {
+                RemoveAllScreens();
+                PushScreen(new MainMenu());
+            }
+        }
+
+        public override void BeginDraw(CustomSpriteBatch g)
+        {
+            VictoryAnimation.BeginDraw(g);
+        }
+
+        public override void Draw(CustomSpriteBatch g)
+        {
+            VictoryAnimation.Draw(g, new Vector2(0, 0));
+        }
+    }
+}
