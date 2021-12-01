9add3bd041dbd16320f7c3d70b45bd4c4668a680
Wargamer <759249+Wargamer3@users.noreply.github.com>
2021-12-01 04:22:53 -0500

    Allow Unit Transforming to change Characters as well

--- Units/Project Eternity Unit Transforming/Unit Transforming.cs
+++ Units/Project Eternity Unit Transforming/Unit Transforming.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Microsoft.Xna.Framework.Content;
+using ProjectEternity.Core.Characters;
 using ProjectEternity.Core.Item;
 using ProjectEternity.Core.Skill;
 using ProjectEternity.Core.Units.Normal;
@@ -41,12 +42,14 @@ namespace ProjectEternity.Core.Units.Transforming
         }
 
         public TransformationInformations[] ArrayTransformingUnit;
-        public int ActiveUnit;
+        public int ActiveUnitIndex;
         public int TimeRemaining;
         public bool PermanentTransformation;
 
         private double HPPercentage;
         private double ENPercentage;
+
+        private Character[] ArrayOriginalCharacter;
 
         public UnitTransforming()
             : base(null)
@@ -70,8 +73,9 @@ namespace ProjectEternity.Core.Units.Transforming
             PermanentTransformation = false;
             HPPercentage = 1;
             ENPercentage = 1;
-            ActiveUnit = 0;
+            ActiveUnitIndex = 0;
             TimeRemaining = -1;
+            ArrayOriginalCharacter = new Character[0];
 
             FileStream FS = new FileStream("Content/Units/Transforming/" + Name + ".peu", FileMode.Open, FileAccess.Read);
             BinaryReader BR = new BinaryReader(FS, Encoding.UTF8);
@@ -102,7 +106,7 @@ namespace ProjectEternity.Core.Units.Transforming
             {
                 _HP = ArrayTransformingUnit[0].UnitTransformed.MaxHP;
                 _EN = ArrayTransformingUnit[0].UnitTransformed.MaxEN;
-                ChangeUnit(ActiveUnit);
+                ChangeUnit(ActiveUnitIndex);
             }
         }
 
@@ -112,24 +116,46 @@ namespace ProjectEternity.Core.Units.Transforming
             Map = Other.Map;
         }
 
-        public void ChangeUnit(int ActiveUnit)
+        public void ChangeUnit(int NewActiveUnitIndex)
         {//Used to avoid updating HP, EN and PermanentTransformation on Init.
-            if (this.ActiveUnit != ActiveUnit)
+            if (this.ActiveUnitIndex != NewActiveUnitIndex)
             {
                 HPPercentage = HP / (double)MaxHP;
                 ENPercentage = EN / (double)MaxEN;
-                if (ArrayTransformingUnit[ActiveUnit].PermanentTransformation)
+                if (ArrayTransformingUnit[NewActiveUnitIndex].PermanentTransformation)
                     PermanentTransformation = true;
 
-                this.ActiveUnit = ActiveUnit;
+                this.ActiveUnitIndex = NewActiveUnitIndex;
+            }
+            else if (ArrayOriginalCharacter.Length != ArrayCharacterActive.Length)
+            {
+                ArrayOriginalCharacter = new Character[ArrayCharacterActive.Length];
+                for (int C = 0; C < ArrayCharacterActive.Length; ++C)
+                {
+                    ArrayOriginalCharacter[C] = ArrayCharacterActive[C];
+                }
             }
             
-            _UnitStat = ArrayTransformingUnit[ActiveUnit].UnitTransformed.UnitStat;
-            
+            _UnitStat = ArrayTransformingUnit[NewActiveUnitIndex].UnitTransformed.UnitStat;
+
+            if (ArrayCharacterActive.Length > 0)
+            {
+                ArrayCharacterActive[0] = ArrayOriginalCharacter[NewActiveUnitIndex];
+                int NextCharacterChangeIndex = 1;
+                for (int C = 0; C < ArrayOriginalCharacter.Length; ++C)
+                {
+                    if (C == NewActiveUnitIndex)
+                        continue;
+
+                    ArrayCharacterActive[NextCharacterChangeIndex] = ArrayOriginalCharacter[C];
+                    NextCharacterChangeIndex++;
+                }
+            }
+
             _HP = (int)(MaxHP * HPPercentage);
             _EN = (int)(MaxEN * ENPercentage);
-            SpriteMap = ArrayTransformingUnit[ActiveUnit].UnitTransformed.SpriteMap;
-            SpriteUnit = ArrayTransformingUnit[ActiveUnit].UnitTransformed.SpriteUnit;
+            SpriteMap = ArrayTransformingUnit[NewActiveUnitIndex].UnitTransformed.SpriteMap;
+            SpriteUnit = ArrayTransformingUnit[NewActiveUnitIndex].UnitTransformed.SpriteUnit;
         }
 
         public bool CanTransform(int ActiveUnit, Unit CurrentWingmanA, Unit CurrentWingmanB)
@@ -205,18 +231,36 @@ namespace ProjectEternity.Core.Units.Transforming
 
         protected override void DoQuickSave(BinaryWriter BW)
         {
-            BW.Write(ActiveUnit);
+            BW.Write(ActiveUnitIndex);
             BW.Write(TimeRemaining);
             BW.Write(PermanentTransformation);
+            for (int C = 0; C < ArrayOriginalCharacter.Length; ++C)
+            {
+                BW.Write(ArrayOriginalCharacter[C].Name);
+            }
         }
 
         protected override void DoQuickLoad(BinaryReader BR, ContentManager Content)
         {
-            ActiveUnit = BR.ReadInt32();
+            ActiveUnitIndex = BR.ReadInt32();
             TimeRemaining = BR.ReadInt32();
             PermanentTransformation = BR.ReadBoolean();
 
-            ChangeUnit(ActiveUnit);
+            ArrayOriginalCharacter = new Character[ArrayCharacterActive.Length];
+            for (int C = 0; C < ArrayOriginalCharacter.Length; ++C)
+            {
+                string CharacterName = BR.ReadString();
+                for (int C2 = 0; C2 < ArrayCharacterActive.Length; ++C2)
+                {
+                    if (ArrayCharacterActive[C2].Name == CharacterName)
+                    {
+                        ArrayOriginalCharacter[C] = ArrayCharacterActive[C2];
+                        break;
+                    }
+                }
+            }
+
+            ChangeUnit(ActiveUnitIndex);
         }
 
         public override void DoInit()
